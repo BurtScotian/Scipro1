64b1173be3c9a79ff4faa7edc8fe5b61c29c6aa0
david wells <davidwells@davids-laptop.local>
2021-12-09 19:31:08 +0000

    First registration of file

---
+++ auth.py
@@ -0,0 +1,9 @@
+
+# API key
+consumer_key = 'pdYx2eNDB3StF1fv9OZc2OVo1'
+# API secret key
+consumer_secret = 'bjsxjDUEfjSzbaY1q0dIK0PG3XDlrlE62wYyx94w8IACI3mC5v'
+# Access token
+access_token = '1086624594633601025-z5gSm0i0qS3iFk2CHP6hN5cbKzU6aI'
+# Access token secret
+access_token_secret = 'Rwftus4OVgwh8PhGFeMGkhCzWDeTSX3ayxIp1Lk54n6yu'
